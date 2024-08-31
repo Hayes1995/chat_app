13ef713668330b8039bbb037cb003a115387324d
Hayes <mcnelishayes@gmail.com>
2024-08-31 16:31:50 -0400

    commit 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:chat_now/screens/auth.dart';
 import 'dart:ui';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_options.dart';
 
 void main() {
   runApp(const App());
@@ -20,3 +22,5 @@ class App extends StatelessWidget {
         home: const AuthScreen());
   }
 }
+// https://firebase.google.com/docs/flutter/setup?platform=ios
+// one of max's links he provided, i cant seem to fint the NPM one for some reason
